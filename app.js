9ef98be121f48995f22f331175c07d4953dc1276
adwait kulkarni <adwaitkulkarni2211@gmail.com>
2022-01-15 15:13:38 +0530

    getting response json

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express')
-const app = express()
-const axios = require("axios")
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const axios = require("axios");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.end('Hello World!');
+app.post("/addresses", async (req, res) => {
+  const result = await Promise.all(req.body.map(location => {
+      return apiCall(location);
+  }))
+
+  res.json(result);
 });
 
-app.post("/addresses", (req, res) => {
-    let result = []
-    req.body.forEach(location => {
-        axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-            params: {
-                address: location,
-                key: "AIzaSyA5bwbEsAOUMOI4RK2zXcIayG4vjuQSpcw"
-            }
-        }).then((response) => {
-            //console.log(response);
-            result.push(response.data.results[0].geometry.location);
-            console.log(result);
-        }).catch((error) => {
-            console.log(error);
-        })
-    })
-    res.json(result);
-})
-
-const port = 3000
-app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
+const apiCall = (location) => {
+  return new Promise((resolve) => {
+    axios
+      .get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+          address: location,
+          key: "AIzaSyA5bwbEsAOUMOI4RK2zXcIayG4vjuQSpcw",
+        },
+      })
+      .then((response) => {
+        //console.log(response);
+        resolve(response.data.results[0].geometry.location);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
+};
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
